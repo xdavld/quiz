d774e73249e478aeeebc88598e71afb87d620494
David Kreismann <wi22230@lehre.dhbw-stuttgart.de>
2023-10-24 18:36:48 +0200

    test

--- abschlussquiz/src/components/IntroPage/IntroPage.jsx
+++ abschlussquiz/src/components/IntroPage/IntroPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom"; 
 
-import "../IntroPage.scss";
+import "./IntroPage.scss";
 
 const IntroPage = () => {
   const speechBubbles = [
